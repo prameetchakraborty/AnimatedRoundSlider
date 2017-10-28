7dc83e6bb9e548e3743dedcf7bb7f04225c3c45d
prameetchakraborty <prameet@geekyants.com>
2017-10-28 21:30:43 +0530

    Added Clock component

---
+++ src/Clock.js
@@ -0,0 +1,7 @@
+import React, { PureComponent, PropTypes } from 'react';
+import { G, Circle, Text, Line } from 'react-native-svg';
+import range from 'lodash.range';
+
+
+export default class ClockFace extends PureComponent {
+}
