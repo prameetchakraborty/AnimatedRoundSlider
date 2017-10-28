8a94bbc20b8a4558889267602268fbd53d180556
prameetchakraborty <prameet@geekyants.com>
2017-10-28 21:28:40 +0530

    Initial functions for slider

--- src/AnimatedSlider.js
+++ src/AnimatedSlider.js
@@ -14,3 +14,37 @@ function calculateArcColor(index0, segments, gradientColorFrom, gradientColorTo)
         toColor: interpolate((index0 + 1) / segments),
     }
 }
+
+function calculateArcCircle(index0, segments, radius, startAngle0 = 0, angleLength0 = 2 * Math.PI) {
+    // Add 0.0001 to the possible angle so when start = stop angle, whole circle is drawn
+    const startAngle = startAngle0 % (2 * Math.PI);
+    const angleLength = angleLength0 % (2 * Math.PI);
+    const index = index0 + 1;
+    const fromAngle = angleLength / segments * (index - 1) + startAngle;
+    const toAngle = angleLength / segments * index + startAngle;
+    const fromX = radius * Math.sin(fromAngle);
+    const fromY = -radius * Math.cos(fromAngle);
+    const realToX = radius * Math.sin(toAngle);
+    const realToY = -radius * Math.cos(toAngle);
+
+    // add 0.005 to start drawing a little bit earlier so segments stick together
+    const toX = radius * Math.sin(toAngle + 0.005);
+    const toY = -radius * Math.cos(toAngle + 0.005);
+
+    return {
+        fromX,
+        fromY,
+        toX,
+        toY,
+        realToX,
+        realToY,
+    };
+}
+
+function getGradientId(index) {
+    return `gradient${index}`;
+}
+
+export default class CircularSlider extends PureComponent {
+    
+}
