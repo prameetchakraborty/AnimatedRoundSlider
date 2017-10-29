899c1fd97c26e4977412a8f2611152269de324f4
prameetchakraborty <prameet@geekyants.com>
2017-10-29 19:21:03 +0530

    Changes to Main File

--- App.js
+++ App.js
@@ -3,10 +3,28 @@ import { StyleSheet, Text, View } from 'react-native';
 import AnimatedSlider from './src/AnimatedSlider';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startAngle: null,
+      angleLength: null,
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
-        <AnimatedSlider/>
+        <AnimatedSlider 
+          startAngle={this.state.startAngle}
+          angleLength={this.state.angleLength}
+          onUpdate={({ startAngle, angleLength }) => this.setState({ startAngle, angleLength })}
+          segments={5}
+          strokeWidth={40}
+          radius={145}
+          gradientColorFrom="#ff9800"
+          gradientColorTo="#ffcf00"
+          showClockFace
+          clockFaceColor="#9d9d9d"
+          bgCircleColor="#171717"/>
       </View>
     );
   }
