9787d8dd8a2f222911559ebefd4d5bd72cf76328
prameetchakraborty <prameet@geekyants.com>
2017-10-29 17:28:28 +0530

    Added main loading main component

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AnimatedSlider from './src/AnimatedSlider';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <AnimatedSlider/>
       </View>
     );
   }
